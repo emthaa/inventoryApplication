530a315f8c8cfc767da10aa14c9546d38ddf2445
emthaa <110277522+emthaa@users.noreply.github.com>
2024-10-26 00:52:38 +0000

    styled app

--- app.js
+++ app.js
@@ -3,20 +3,21 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+app.use(express.static(__dirname + '/public'));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 //----------------------------------------
 
-const indexRouter = require("./routes/indexRouter"); //gets router
+const indexRouter = require("./routes/indexRouter");
 const gamesRouter = require("./routes/gamesRouter");
 const genresRouter = require("./routes/genresRouter");
 const developersRouter = require("./routes/developersRouter");
 
 
-app.use(express.urlencoded({ extended: true })); //req.body encoder
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/", indexRouter); //mounts router at /
+app.use("/", indexRouter);
 app.use("/", gamesRouter);
 app.use("/", genresRouter);
 app.use("/", developersRouter);
